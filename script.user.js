cff939d4378ff17c435f1a94b1cc1ed7818dae43
1grkarelkhagr1 <280758673+1grkarelkhagr1@users.noreply.github.com>
2026-04-30 22:08:35 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Weekly Staff
 // @namespace    browser-tools
-// @version      1.2
+// @version      1.3
 // @description  Reports
 // @match        https://www.scheduleit.com/cloud/std2/*
 // @match        https://www.scheduleit.com/cloud/std2/reports_create.php*
@@ -17,7 +17,7 @@
 // ==/UserScript==
 
 (() => {
-  /* ================= CONFIG ================= =*/
+  /* ================= CONFIG =================*/
   const MOBILE_SCALE = 0.6;
   const FILE_LABEL = "Weekly_Staff";
   const EMAIL_FILE_LABEL = "Weekly_Staff_Email_List";
